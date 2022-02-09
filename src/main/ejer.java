2f416b30bf1937a389e046f1b710d62f3959ff16
luis miguel <luismrocha132@gmail.com>
2022-02-08 21:15:43 -0500

    suma de dos numero

---
+++ src/main/ejer.java
@@ -0,0 +1,12 @@
+package main;
+
+public class ejer {
+    public static void main(String[] args) {
+        var n = 1;
+        var n2 = 2 ;
+      int   suma = n + n2 ;
+        System.out.println(suma);
+    }
+
+}
+
