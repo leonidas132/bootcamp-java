693fc064311ad9b44f6df9e9461a8d9fe863d702
luis miguel <luismrocha132@gmail.com>
2022-02-23 09:48:25 -0500

    ejercicios de practicas

---
+++ src/semana1/condicionales.java
@@ -0,0 +1,17 @@
+package semana1;
+
+public class condicionales {
+
+
+    public static void main(String[] args){
+      /*  byte n = 15;
+        if(n==10){
+            System.out.println("el numnero corresponde a: "+n);
+        }else
+            System.out.println("numero incorrecto");*/
+        int n1 = 20;
+        int n2 = 10;
+        //int n3 = n1 > n2;
+        //System.out.println(n3);
+    }
+}
