dacc1cdc3d0c4b3bcbc43f4adfbe1e8f7fdf79dc
luis miguel <luismrocha132@gmail.com>
2022-02-23 09:51:01 -0500

    ejercicios

--- src/semana1/condicionales.java
+++ src/semana1/condicionales.java
@@ -9,9 +9,5 @@ public class condicionales {
             System.out.println("el numnero corresponde a: "+n);
         }else
             System.out.println("numero incorrecto");*/
-        int n1 = 20;
-        int n2 = 10;
-        //int n3 = n1 > n2;
-        //System.out.println(n3);
     }
 }
