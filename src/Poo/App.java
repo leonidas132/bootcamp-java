e635ca156e6ceb63111f1013149fbe37164f20f6
luis miguel <luismrocha132@gmail.com>
2022-04-22 18:54:56 -0500

    si

---
+++ src/Poo/App.java
@@ -0,0 +1,7 @@
+package Poo;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Poo/ExcipcionPersonalizada.java
@@ -0,0 +1,12 @@
+package Poo;
+
+public class ExcipcionPersonalizada extends Exception {
+    private static final long serialVersionUID = 1L;
+    private final String [ ] ERROR_DESCRIPCION = {"ERROR EN LINEA DE FECHA ", "ERROR DESCONOCIDO , CONTACTE "};
+    private int codigoError;
+
+
+
+
+    }
+}
